a9590a185620c58e6139e7e570244762b987a02c
KsenyaKoss <roxenn34@gmail.com>
2023-03-22 14:42:51 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+
+function changingColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopChangingColor() {
+    clearInterval(timerId); 
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', () => timerId = setInterval(changingColor, 1000));
+
+stopBtn.addEventListener('click',stopChangingColor);
