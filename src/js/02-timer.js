9a5897d82f1aa0c94b359d0d78dfdbdad67855d5
KsenyaKoss <roxenn34@gmail.com>
2023-03-23 11:22:37 +0100

    task-2-complete

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+import Notiflix from 'notiflix'
+
+
+const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+startBtn.disabled = true;
+let TIME_DIFF = 0
+let timerId = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if(selectedDates[0] <= new Date()) {
+        Notiflix.Notify.failure("Please choose a date in the future")} 
+         else { startBtn.disabled = false;
+            TIME_DIFF = selectedDates[0];
+            console.log(selectedDates[0]);
+            console.log(TIME_DIFF);
+         }
+    },
+  };
+
+ flatpickr("#datetime-picker", options);
+
+startBtn.addEventListener('click', () =>{
+    timerId = setInterval(()=>{
+        startCounter(TIME_DIFF)
+    }, 1000)
+});
+
+function startCounter(TIME_DIFF){
+   let ms = TIME_DIFF - new Date();
+   if(ms > 0 ){
+      changeTextContent(convertMs(ms))}
+      else {
+          clearInterval(timerId)};      
+};
+
+
+function addLeadingZero(value){
+    if(value < 10){
+    return String(value).padStart(2, '0')};
+    return value;
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    return { days, hours, minutes, seconds };
+  }
+
+function changeTextContent ({days, hours, minutes, seconds}) {
+    daysEl.textContent = days;
+    hoursEl.textContent = hours;
+    minutesEl.textContent = minutes;
+    secondsEl.textContent = seconds;
+ }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// import Notiflix from 'notiflix'
+// const refs = {
+//   submitBtn: document.querySelector('[type="submit"]'),
+//   delayEl: document.querySelector('[name="delay"]'),
+//   stepEl: document.querySelector('[name="step"]'),
+//   amountEl: document.querySelector('[name="amount"]'),
+// }
+
+// refs.submitBtn.addEventListener('click', ()=>{
+//   setInterval(createPromise(), refs.amountEl.value)
+// })
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     Notiflix.Notify.success('Sol lucet omnibus')
+//   } else {
+//     Notiflix.Notify.failure("Please choose a date in the future")
+//   }
+// }
+
+
