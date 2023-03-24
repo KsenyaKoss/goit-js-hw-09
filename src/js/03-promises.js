dc4da1ad660004a1bc6f7d2d222fe87df19b09f7
KsenyaKoss <roxenn34@gmail.com>
2023-03-24 23:48:23 +0100

    last fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,16 @@
 import Notiflix from 'notiflix'
 
   const submitBtn = document.querySelector('[type="submit"]');
+  
 
 submitBtn.addEventListener('click', (ev)=>{
      ev.preventDefault();
+     let position = 1;
      let delay = parseInt(document.querySelector('[name="delay"]').value) || 0;
      const step = parseInt(document.querySelector('[name="step"]').value) || 0;
      const amount = parseInt(document.querySelector('[name="amount"]').value);
   for (let index = 0; index < amount; index++) {
-    let position = 1;
+    
      createPromise(position, delay)
      .then(({ position, delay }) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -17,7 +19,7 @@ submitBtn.addEventListener('click', (ev)=>{
       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
      delay += step;
-     position +=1;
+     position += 1;
   }  
 })
 
